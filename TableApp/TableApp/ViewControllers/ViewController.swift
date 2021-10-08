7a0d19e71b8caa88cd922f467305fb119a0b4ebf
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-08 17:11:24 +0300

    Initial Project commit

---
+++ TableApp/TableApp/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by Сергей Гнидь on 08.10.2021.
+//
+
+import UIKit
+
+class FirstUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
